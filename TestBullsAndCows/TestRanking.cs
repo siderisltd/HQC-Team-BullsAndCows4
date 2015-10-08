50c432b3e5ec609b28bdfe17f13a6844bd1fc8ec
RossyB <rossy@compass-bg.net>
2015-10-08 22:57:07 +0300

    Add more tests

---
+++ TestBullsAndCows/TestRanking.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using BullsAndCowsGame.Interfaces;
+using BullsAndCowsGame.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestBullsAndCows
+{
+    [TestClass]
+    public class TestRanking
+    {
+        [TestMethod]
+        public void TestRankingMethod()
+        {
+            var ranklist = new Ranking<IPlayer>();
+            var expect = 0;
+
+            Assert.AreEqual(expect, ranklist.Count);
+        }
+
+        [TestMethod]
+        public void TestRankingMethodAdd()
+        {
+            var humanPlayer = new HumanPlayer("Ivan", "3456");
+            var ranklist = new Ranking<IPlayer>();
+            ranklist.Add(humanPlayer);
+            var expect = 1;
+
+            Assert.AreEqual(expect, ranklist.Count);
+        }
+
+        [TestMethod]
+        public void TestRankingMethodMoveNext()
+        {
+            var humanPlayer = new HumanPlayer("Ivan", "3456");
+            var ranklist = new Ranking<IPlayer>();
+            ranklist.Add(humanPlayer);
+            var move = ranklist.MoveNext();
+
+            Assert.IsTrue(move);
+        }
+
+        [TestMethod]
+        public void TestRankingMethodDispose()
+        {
+            var humanPlayer = new HumanPlayer("Ivan", "3456");
+            var ranklist = new Ranking<IPlayer>();
+            ranklist.Add(humanPlayer);
+
+            ranklist.Dispose();
+
+        ////TODO
+        }
+
+    }
+}
